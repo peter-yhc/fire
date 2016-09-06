b79e74b6032bae79bc538b160e0915afff596416
Peter Chen <peter.chen2@mail.mcgill.ca>
2016-09-06 23:12:30 +1000

    Added very basic HOT table on expense page.

--- public/app/expenses/expenses.component.ts
+++ public/app/expenses/expenses.component.ts
@@ -1,8 +1,66 @@
-import {Component} from "@angular/core";
+import {Component, AfterViewInit} from "@angular/core";
+
+declare var Handsontable: any;
 
 @Component({
-    templateUrl: 'app/expenses/expenses.component.html'
+    templateUrl: 'app/expenses/expenses.component.html',
+    styleUrls: ['app/expenses/expenses.component.css']
 })
-export class ExpensesComponent {
+export class ExpensesComponent implements AfterViewInit {
 
+    private data;
+    private container;
+    private hot;
+
+    ngAfterViewInit() {
+        this.data = [
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+        ];
+
+        this.container = document.getElementById('example');
+        this.hot = new Handsontable(this.container, {
+            data: this.data,
+            rowHeaderWidth: 150,
+            rowHeaders: [
+                'Mortgage / Rent',
+                'Home Insurance',
+                'Property Tax',
+                'Health Insurance',
+                'Food & Groceries',
+                'Electric',
+                'Natural Gas',
+                'Cable & Internet',
+                'Cell',
+                'Trash',
+                'Car Insurance',
+                'Car Maintenance',
+                'Car Purchase',
+                'Car Petrol',
+                'Home Repairs',
+                'Pet',
+                'Misc',
+                'Total'
+            ],
+            colHeaders: [
+                'Necessary', 'Expenses', 'Costs', 'Budget', 'Daily', 'Monthly', 'Annually', 'Savings', 'FI'
+            ]
+        });
+    }
 }
--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -24,7 +24,8 @@
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
             // other libraries
             'rxjs': 'npm:rxjs',
-            'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api'
+            'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+            'Handsontable': 'npm:handsontable'
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
@@ -37,6 +38,9 @@
             },
             'angular2-in-memory-web-api': {
                 main: './index.js',
+                defaultExtension: 'js'
+            },
+            'handsontable': {
                 defaultExtension: 'js'
             }
         }
