e7857932fdc2fbb09d5205e708c94ffa86e292aa
Peter Chen <peter.chen2@mail.mcgill.ca>
2016-09-18 20:31:51 +1000

    Transposed net-worth table structure.

--- public/app/net-worth/net-worth.component.ts
+++ public/app/net-worth/net-worth.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from "@angular/core";
 
 @Component({
-    templateUrl: 'app/net-worth/net-worth.component.html'
+    templateUrl: "app/net-worth/net-worth.component.html",
+    styleUrls: ["app/net-worth/net-worth.component.css"]
+
 })
 export class NetWorthComponent implements OnInit {
     private rowData;
@@ -9,21 +11,30 @@ export class NetWorthComponent implements OnInit {
 
     ngOnInit() {
         this.columnDefs = [
-            {headerName: 'Month', field: 'month'},
-            {headerName: 'Net Worth', field: 'netWorth'},
-            {headerName: 'Liquid', field: 'liquid'},
-            {headerName: 'Income', field: 'income'},
-            {headerName: 'Expenses', field: 'expenses'},
-            {headerName: 'Investment Acc 1', field: 'investmentAcc1'},
-            {headerName: 'Investment Acc 2', field: 'investmentAcc2'},
-            {headerName: 'Investment Acc 3', field: 'investmentAcc3'},
-            {headerName: 'Savings 1', field: 'savings1'}
+            {headerName: "Account", field: "account"},
+            {headerName: "Jan 2016", field: "month1"},
+            {headerName: "Feb 2016", field: "month2"},
+            {headerName: "Mar 2016", field: "month3"},
+            {headerName: "Apr 2016", field: "month4"},
+            {headerName: "May 2016", field: "month5"},
+            {headerName: "Jun 2016", field: "month6"},
+            {headerName: "Jul 2016", field: "month7"},
+            {headerName: "Aug 2016", field: "month8"},
+            {headerName: "Sep 2016", field: "month9"},
+            {headerName: "Oct 2016", field: "month10"},
+            {headerName: "Nov 2016", field: "month11"},
+            {headerName: "Dec 2016", field: "month12"}
         ];
 
         this.rowData = [
-            {month: "2016-01", netWorth: "20000", liquid: "10000", income: "1500", expenses: "500", investmentAcc1: "400", investmentAcc2: "600", investmentAcc3: "200", savings1: "500"},
-            {month: "2016-02", netWorth: "20000", liquid: "10000", income: "1500", expenses: "500", investmentAcc1: "400", investmentAcc2: "600", investmentAcc3: "200", savings1: "500"},
-            {month: "2016-03", netWorth: "20000", liquid: "10000", income: "1500", expenses: "500", investmentAcc1: "400", investmentAcc2: "600", investmentAcc3: "200", savings1: "500"},
+            {account: "Liquid", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
+            {account: "Income", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
+            {account: "Expenses", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
+            {account: "Invest1", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
+            {account: "Invest2", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
+            {account: "Equity1", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
+            {account: "Debt1", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
+            {account: "Net Worth", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
         ];
     }
 }
