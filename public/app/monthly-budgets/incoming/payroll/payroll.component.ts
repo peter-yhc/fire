6d2cc85d48ae7d558cbde224ccf4eee5d732979d
Peter Chen <peter.chen2@mail.mcgill.ca>
2016-09-29 15:12:14 +0100

    Added currency filters to payroll table.

--- public/app/monthly-budgets/incoming/payroll/payroll.component.ts
+++ public/app/monthly-budgets/incoming/payroll/payroll.component.ts
@@ -11,17 +11,17 @@ import {Payroll} from "./Payroll";
     ]
 })
 export class PayrollComponent implements OnInit {
-    @Input() monthId: number;
+    @Input() monthId:number;
 
     private payrollData;
     private columnDefs;
     private payrollService;
 
-    constructor(payrollService: PayrollService) {
+    constructor(payrollService:PayrollService) {
         this.payrollService = payrollService;
     }
 
-    ngOnInit(): void {
+    ngOnInit():void {
         this.columnDefs = [
             {headerName: "Paycheck", field: "rowHeader"},
             {headerName: "Pay Period 1", field: "1"},
@@ -31,14 +31,14 @@ export class PayrollComponent implements OnInit {
         ];
 
         this.payrollData = [
-            {rowHeader: "Total", "1": "250", "2": "250"},
-            {rowHeader: "Tax", "1": "250", "2": "250"},
-            {rowHeader: "Net", "1": "1532", "2": "1532"},
-            {rowHeader: "Superannuation", "1": "150", "2": "150"}
+            {rowHeader: "Total"},
+            {rowHeader: "Tax"},
+            {rowHeader: "Net"},
+            {rowHeader: "Retirement"}
         ];
 
         let payrollList = this.payrollService.get(2016, this.monthId);
-        payrollList.forEach((payroll : Payroll, index) => {
+        payrollList.forEach((payroll:Payroll, index) => {
             this.payrollData[0][index] = payroll.totalAmount;
             this.payrollData[1][index] = payroll.taxedAmount;
             this.payrollData[2][index] = payroll.netPayment;
