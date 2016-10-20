6559577ca7a17d02dda0e2214d60dff6d61b8959
Peter Chen <peter.chen2@mail.mcgill.ca>
2016-10-20 22:37:43 +1100

    Mongodb connection config

--- fire-server/src/main/java/org/pyhc/fire/Application.java
+++ fire-server/src/main/java/org/pyhc/fire/Application.java
@@ -3,8 +3,7 @@ package org.pyhc.fire;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication
+@SpringBootApplicationi
 public class Application {
 
     public static void main(String[] args) {
---
+++ fire-server/src/main/java/org/pyhc/fire/config/DatabaseConfiguration.java
@@ -0,0 +1,27 @@
+package org.pyhc.fire.config;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "org.pyhc.fire.repository")
+public class DatabaseConfiguration extends AbstractMongoConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "fire_db";
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient("127.0.0.1", 27017);
+    }
+
+    @Override
+    protected String getMappingBasePackage() {
+        return "org.pyhc.fire";
+    }
+}
