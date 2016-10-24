5b881953c9e9cb71f0065dfea17ebcd2e93001f0
Peter Chen <peter.chen2@mail.mcgill.ca>
2016-10-25 10:14:21 +1100

    Fix depedency tree.

--- fire-server-core/src/main/java/org/pyhc/fire/payroll/PayrollRepository.java
+++ fire-server-core/src/main/java/org/pyhc/fire/payroll/PayrollRepository.java
@@ -1,7 +1,9 @@
 package org.pyhc.fire.payroll;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource
 public interface PayrollRepository extends MongoRepository<Payroll, String> {
 
 }
