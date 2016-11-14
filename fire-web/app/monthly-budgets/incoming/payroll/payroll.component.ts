810abe36e7eed1724888fd12e4ad822cc90dfb25
Peter Chen <peter.chen2@mail.mcgill.ca>
2016-11-14 22:47:09 +1100

    Transpose paycheck table in monthly-budget payroll component.

--- fire-web/app/monthly-budgets/incoming/payroll/payroll.component.ts
+++ fire-web/app/monthly-budgets/incoming/payroll/payroll.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit, Input} from "@angular/core";
 import {PayrollService} from "./payroll.service";
-import {Payroll} from "./Payroll";
 
 @Component({
     selector: "payroll-component",
@@ -21,30 +20,21 @@ export class PayrollComponent implements OnInit {
         this.payrollService = payrollService;
     }
 
+    logData():void {
+        console.log(JSON.stringify(this.payrollData));
+    }
+
     ngOnInit(): void {
         this.columnDefs = [
-            {headerName: "Paycheck", field: "rowHeader"},
-            {headerName: "Pay Period 1", field: "0"},
-            {headerName: "Pay Period 2", field: "1"},
-            {headerName: "Pay Period 3", field: "2"},
-            {headerName: "Pay Period 4", field: "3"}
-        ];
-
-        this.payrollData = [
-            {rowHeader: "Total"},
-            {rowHeader: "Tax"},
-            {rowHeader: "Net"},
-            {rowHeader: "Retirement"}
+            {headerName: "Paycheck", field: "payPeriod"},
+            {headerName: "Tax", field: "taxedAmount"},
+            {headerName: "Net", field: "netPayment"},
+            {headerName: "Retirement", field: "retirementPlan"},
+            {headerName: "Total", field: "totalAmount"}
         ];
 
         this.payrollService.get(2016, this.monthId).subscribe(
-            payrolls => (payrolls.forEach((payroll: Payroll, index) => {
-                    this.payrollData[0][index] = payroll.totalAmount;
-                    this.payrollData[1][index] = payroll.taxedAmount;
-                    this.payrollData[2][index] = payroll.netPayment;
-                    this.payrollData[3][index] = payroll.retirementPlan;
-                })
-            )
+            payrolls => this.payrollData = payrolls
         );
     }
 }
