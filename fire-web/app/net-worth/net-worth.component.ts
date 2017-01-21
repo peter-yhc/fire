7c9bddaaf1ca27b95ff5d363d67a6596a848e212
Peter Chen <peter.chen2@mail.mcgill.ca>
2017-01-21 11:45:26 +0000

    Preliminary implementation of networth page.

--- fire-web/app/net-worth/net-worth.component.ts
+++ fire-web/app/net-worth/net-worth.component.ts
@@ -1,40 +1,71 @@
 import {Component, OnInit} from "@angular/core";
+import {SelectItem} from "primeng/primeng";
 
 @Component({
     templateUrl: "app/net-worth/net-worth.component.html",
-    styleUrls: ["app/net-worth/net-worth.component.css"]
-
+    styleUrls: ["app/net-worth/net-worth.component.css"],
 })
 export class NetWorthComponent implements OnInit {
     private rowData;
     private columnDefs;
+    private columnOptions:SelectItem[];
 
     ngOnInit() {
         this.columnDefs = [
-            {headerName: "Account", field: "account"},
-            {headerName: "Jan 2016", field: "month1"},
-            {headerName: "Feb 2016", field: "month2"},
-            {headerName: "Mar 2016", field: "month3"},
-            {headerName: "Apr 2016", field: "month4"},
-            {headerName: "May 2016", field: "month5"},
-            {headerName: "Jun 2016", field: "month6"},
-            {headerName: "Jul 2016", field: "month7"},
-            {headerName: "Aug 2016", field: "month8"},
-            {headerName: "Sep 2016", field: "month9"},
-            {headerName: "Oct 2016", field: "month10"},
-            {headerName: "Nov 2016", field: "month11"},
-            {headerName: "Dec 2016", field: "month12"}
+            {headerName: "Month", field: "month"},
+            {headerName: "Net Worth", field: "netWorth"},
+            {headerName: "Liquid", field: "liquid"},
+            {headerName: "Invested", field: "invested"},
+            {headerName: "Home Equity", field: "homeEquity"},
+            {headerName: "Income", field: "income"}
         ];
 
         this.rowData = [
-            {account: "Liquid", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
-            {account: "Income", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
-            {account: "Expenses", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
-            {account: "Invest1", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
-            {account: "Invest2", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
-            {account: "Equity1", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
-            {account: "Debt1", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
-            {account: "Net Worth", month1: 10000, month2: 10000, month3: 10000, month4: 10000, month5: 10000, month6: 10000, month7: 10000, month8: 10000, month9: 10000, month10: 10000, month11: 10000, month12: 10000},
+            {
+                month: "2016-01",
+                netWorth: 12302.21,
+                "liquid": 8454.23,
+                "invested": 64972,
+                "homeEquity": 3522,
+                "income": 23432
+            },
+            {
+                month: "2016-02",
+                netWorth: 12302.21,
+                "liquid": 8454.23,
+                "invested": 64972,
+                "homeEquity": 3522,
+                "income": 23432
+            },
+            {
+                month: "2016-03",
+                netWorth: 12302.21,
+                "liquid": 8454.23,
+                "invested": 64972,
+                "homeEquity": 3522,
+                "income": 23432
+            },
+            {
+                month: "2016-04",
+                netWorth: 12302.21,
+                "liquid": 8454.23,
+                "invested": 64972,
+                "homeEquity": 3522,
+                "income": 23432
+            },
+            {
+                month: "2016-05",
+                netWorth: 12302.21,
+                "liquid": 8454.23,
+                "invested": 64972,
+                "homeEquity": 3522,
+                "income": 23432
+            },
         ];
+
+        this.columnOptions = [];
+        for (let i = 0; i < this.columnDefs.length; i++) {
+            this.columnOptions.push({label: this.columnDefs[i].headerName, value: this.columnDefs[i]});
+        }
     }
 }
--- fire-web/app/net-worth/net-worth.module.ts
+++ fire-web/app/net-worth/net-worth.module.ts
@@ -1,12 +1,12 @@
-import {NgModule} from "@angular/core";
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 import {NetWorthComponent} from "./net-worth.component";
 import {CommonModule} from "@angular/common";
-import {DataTableModule} from "primeng/components/datatable/datatable";
-import {SharedModule} from "primeng/components/common/shared";
+import {FormsModule} from "@angular/forms";
+import {MultiSelectModule, DataTableModule, SharedModule} from "primeng/primeng";
 
 @NgModule({
     imports: [
-        CommonModule, DataTableModule, SharedModule
+        CommonModule, DataTableModule, SharedModule, MultiSelectModule, FormsModule
     ],
     declarations: [
         NetWorthComponent
